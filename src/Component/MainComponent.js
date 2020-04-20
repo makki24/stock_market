cfa52a934d094218bbee248a6c967cb1d2528a70
Syed Maqthyar <maqhti@gmail.com>
2020-04-20 08:24:30 +0530

    initial setup

---
+++ src/Component/MainComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class MainComponent extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                abi to kuch nahi
+            </div>
+        )
+    }
+}
+
+export default MainComponent;
